4ae4590b3b68c60c49b05a2566154180b9cb4ebf
cybertekschool <github@cybertekschool.com>
2019-12-19 11:22:44 -0500

    background and smoke test feature file

--- src/test/java/com/vytrack/utilities/Driver.java
+++ src/test/java/com/vytrack/utilities/Driver.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import sun.swing.StringUIClientPropertyKey;
 
 public class Driver {
     private static WebDriver driver;
