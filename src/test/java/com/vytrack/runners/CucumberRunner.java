7e1b3b2f97be3df69106465ae05f7f68baca7174
cybertekschool <github@cybertekschool.com>
2019-12-19 10:57:09 -0500

    background and smoke test feature file

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -8,8 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "com/vytrack/step_definitions",
-        dryRun = false,
-        tags = "@store_manager",
+        dryRun = true,
+        tags = "@smoke_test",
         plugin = {"html:target/default-cucumber-reports"}
 )
 public class CucumberRunner {
