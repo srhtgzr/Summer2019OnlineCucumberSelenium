70668c73cb84b4f982222e5b168e8404e10007d1
cybertekschool <github@cybertekschool.com>
2019-12-19 12:31:23 -0500

    added screenshot

--- src/test/java/com/vytrack/step_definitions/Hook.java
+++ src/test/java/com/vytrack/step_definitions/Hook.java
@@ -4,6 +4,8 @@ import com.vytrack.utilities.Driver;
 import io.cucumber.core.api.Scenario;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hook {
 
@@ -19,6 +21,8 @@ public class Hook {
         //if test failed - do this
         if(scenario.isFailed()){
             System.out.println("Test failed!");
+            byte[] screenshot = ((TakesScreenshot)Driver.get()).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
         }else{
             System.out.println("Cleanup!");
             System.out.println("Test completed!");
