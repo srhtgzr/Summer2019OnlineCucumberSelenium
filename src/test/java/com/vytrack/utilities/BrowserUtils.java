0d7a3785fe85ff5a568002e27db7413b1e730552
cybertekschool <github@cybertekschool.com>
2019-12-20 14:27:15 -0500

    added new method for converting list of webelements into list of strings

--- src/test/java/com/vytrack/utilities/BrowserUtils.java
+++ src/test/java/com/vytrack/utilities/BrowserUtils.java
@@ -8,7 +8,9 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.function.Function;
 
 public class BrowserUtils {
@@ -161,6 +163,20 @@ public class BrowserUtils {
         WebDriverWait wait = new WebDriverWait(Driver.get(), 10);
         wait.until(ExpectedConditions.titleIs(pageTitle));
 
+    }
+
+    /**
+     * This method will convert list of WebElements into list of string
+     *
+     * @param listOfWebElements
+     * @return list of strings
+     */
+    public static List<String> getListOfString(List<WebElement> listOfWebElements) {
+        List<String> listOfStrings = new ArrayList<>();
+        for (WebElement element : listOfWebElements) {
+            listOfStrings.add(element.getText().trim());
+        }
+        return listOfStrings;
     }
 
 }
