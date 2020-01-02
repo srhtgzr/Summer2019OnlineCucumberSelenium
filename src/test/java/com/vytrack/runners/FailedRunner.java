d854f2880baa992fb395a5be50f0196e0617be2a
cybertekschool <github@cybertekschool.com>
2020-01-02 13:10:59 -0500

    fixed smoke test typo

--- src/test/java/com/vytrack/runners/FailedRunner.java
+++ src/test/java/com/vytrack/runners/FailedRunner.java
@@ -8,8 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
         features = "@target/rerun.txt",
         glue = "com/vytrack/step_definitions",
-        plugin = {"html:target/default-cucumber-reports",
-                  "json:target/cucumber.json"
+        plugin = {"html:target/rerun-default-cucumber-reports",
         }
 )
 public class FailedRunner {
